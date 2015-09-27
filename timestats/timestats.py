1024959a3a2e1e9add513fa556113ca7ffd012f2
Jim Haughwout <jim.haughwout@gmail.com>
2015-09-27 09:48:06 -0400

    Mode defensive programming

--- timestats/timestats.py
+++ timestats/timestats.py
@@ -1,10 +1,30 @@
 import datetime as dt
 import pytz
-from utils.validation import validate_operand, validate_result
+#from utils.validation import validate_operand, validate_result
 
+def validate_operand(operand):
+    """TODO"""
+    if not isinstance(operand, list):
+        raise TypeError('unsupported operand for timestat, requires list of datetime objects: %r is %s' %
+         (operand, type(operand)))
+    elif not len(operand) > 0:
+        raise ValueError('must have non-zero len list for timestat, passed: %r' % operand)
+
+def validate_result(answer_dt):
+    """TODO docs + comment why conditional and not assertion and why never happen"""
+    if not isinstance(answer_dt, dt.datetime):
+        raise TypeError('non-datetime object in list: %r' % answer_dt)  
+    if not answer_dt.tzinfo:
+        return answer_dt
+    else:
+        return answer_dt.astimezone(pytz.utc)
 
 def mean(dt_list):
-    """TODO"""
+    """
+    TODO
+    will raise TypeError if any item in list is not a datetime
+    TODO if tz, convert to utc as part of validate answer
+    """
     validate_operand(dt_list)
     list_size = len(dt_list)
 
@@ -24,14 +44,23 @@ def mean(dt_list):
         delta = delta_total / float(list_size)
         mean_dt = base_dt + dt.timedelta(seconds=delta)
     
-    validate_result(mean_dt, "timestats.mean")
-    return mean_dt
+    return validate_result(mean_dt)
 
 
 def median(dt_list):
-    """TODO"""
+    """
+    TODO
+    will raise TypeError if any item in list is not a datetime
+    return middlemost (not always UTC)
+    TODO if tz, convert to utc as part of validate answer
+    """
     validate_operand(dt_list)
-    sorted_dt_list = sorted(dt_list)
+
+    try:
+        sorted_dt_list = sorted(dt_list)
+    except TypeError:
+        raise
+    
     list_size = len(sorted_dt_list)
 
     if list_size == 1:
@@ -47,14 +76,22 @@ def median(dt_list):
         middle = [sorted_dt_list[lower], sorted_dt_list[upper]]
         median_dt = mean(middle)
 
-    validate_result(median, "timestats.median")
-    return median_dt
+    #validate_result(median, "timestats.median")
+    return validate_result(median_dt)
 
-
+# TESTS
+# DTs  with TZ - show succeed, regardless of mixed TZs
 nyc = dt.datetime(2014, 1, 1, 12, 0, 0, tzinfo=pytz.timezone('America/New_York'))
 london = dt.datetime(2014, 1, 1, 12, 0, 0, tzinfo=pytz.timezone('Europe/London'))
 singapore = dt.datetime(2014, 1, 1, 12, 0, 0, tzinfo=pytz.timezone('Asia/Singapore'))
 utc = dt.datetime(2014, 1, 1, 12, 0, 0, tzinfo=pytz.utc)
+
+t1 = [london, nyc, singapore, utc]
+t2 = [london, nyc, singapore]
+t3 = [london, nyc, london]
+t4 = [london, nyc]
+
+# Naive DTs - should succeed
 naive_1 = dt.datetime(2015, 9, 10, 12, 0, 0)
 naive_2 = dt.datetime(2015, 9, 30, 12, 0, 0)
 naive_3 = dt.datetime(2015, 9, 22, 12, 0, 0)
@@ -62,38 +99,31 @@ naive_4 = dt.datetime(2015, 9, 12, 12, 0, 0)
 
 n1 = [naive_1, naive_2, naive_3]
 
-
-#print "london:    ", london
-#print "nyc:       ", nyc
-#print "singapore: ", singapore
-#print "utc:       ", utc
-
-t1 = [london, nyc, singapore, utc]
-t2 = [london, nyc, singapore]
-t3 = [london, nyc, london]
-t4 = [london, nyc]
-
+# Mixed DTs - should fail
 foo = [london, naive_1]
 
-#print "naive 01:     ", naive_1
-#print "naive 02:     ", naive_2
-#print "naive 03:     ", naive_3
-#print "naive 04:     ", naive_4
+# Non lists - all should fail
+a_set = set([0])
+an_int = 1
+a_float = 2.0
+a_str = "three"
+a_tuple = (4, 5)
 
-k = t1
+# Zero len list - should fail
+z = []
 
-print
-for entry in k:
-    print entry
+# Mixed list
+m1 = [london, an_int]
 
-print "\nMedian is: %s\nMean is:   %s\n" % (median(k), mean(k)) 
+# RUN IT
+"""
+k = foo
 
-print "Is 1 valid?: %s" % valid(1)
-print "Is 'a' valid?: %s" % valid('a')
-print "Is t1 valid?: %s" % valid(t1)
-print "Is n1 valid?: %s" % valid(n1)
-print "Is foo valid?: %s" % valid(foo)
+if isinstance(k, list):
+    print
+    for entry in k:
+        print entry
 
-
-#print "\nBreak it 1: %s" % median(1)
-#print "\nBreak it 1: %s" % median('a')
+print "\nMean is: %s" % (mean(k)) 
+print "Median is:   %s\n" % (median(k)) 
+"""
